4eff8b93c890505ac8e8e281ac1a3ce09d9a0a03
MonseCastanon <monsecastanon023@gmail.com>
2025-03-03 15:55:03 -0600

    Filtro Actualización
    Actualización en rol

--- index.js
+++ index.js
@@ -7,8 +7,15 @@ const authRoutes = require('./routes/auth.js'); // Importa las rutas de autentic
 
 
 const adminRoutes = require('./routes/adminRoutes.js') // Importa las rutas de admin
+<<<<<<< HEAD
 const operatorRoutes = require('./routes/operatorRoutes.js'); // Importa las rutas de operador
 const activityRoutes = require('./routes/activityRoutes.js'); // Importa las rutas de activity
+=======
+const authRoutes = require('./routes/auth.js'); // Importa las rutas de autenticación
+const operatorRoutes = require('./routes/operatorRoutes.js');
+const registerRoutes = require('./routes/registerRoutes.js');
+const filtroRoutes = require('./routes/filtrosRoutes.js');
+>>>>>>> 08e6ce1 (Filtro Actualización)
 
 const app = express();
 
@@ -20,9 +27,13 @@ app.use(express.urlencoded({ extended: true }));
 
 //Rutas
 app.use('/auth', authRoutes); // Monta las rutas bajo el prefijo "/auth"
+<<<<<<< HEAD
 
 app.use('/admin', adminRoutes); // Monta las rutas bajo el prefijo "/admin"
 app.use('/operator', operatorRoutes); // Monta las rutas bajo el prefijo "/operator"
 app.use('/activity',activityRoutes); // Monta las rutas bajo el prefijo "/activity" 
+=======
+app.use('/filtros', filtroRoutes); // Monta las rutas bajo el prefijo "/filtros"
+>>>>>>> 08e6ce1 (Filtro Actualización)
 
 module.exports = app;
