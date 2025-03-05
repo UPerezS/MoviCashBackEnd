d2d1a768ba7b6bd57c58a846c4c196c1ddd504b6
USER1FR0 <f51783145@gmail.com>
2025-03-04 22:40:08 -0600

    feat/Buscar Actividades por Rol,Fecha,Nombre

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const authRoutes = require('./routes/auth.js'); // Importa las rutas de autentic
 const registerRoutes = require('./routes/registerRoutes') // Importa las rutas bajo el prefijo /register
 const adminRoutes = require('./routes/adminRoutes.js') // Importa las rutas de admin
 const operatorRoutes = require('./routes/operatorRoutes.js'); // Importa las rutas de operador
-
+const activityRoutes = require('./routes/activityRoutes.js'); // Importa las rutas de activity
 
 const app = express();
 
@@ -22,7 +22,6 @@ app.use('/auth', authRoutes); // Monta las rutas bajo el prefijo "/auth"
 app.use('/register',registerRoutes);
 app.use('/admin', adminRoutes); // Monta las rutas bajo el prefijo "/admin"
 app.use('/operator', operatorRoutes); // Monta las rutas bajo el prefijo "/operator"
-
-
+app.use('/activity',activityRoutes); // Monta las rutas bajo el prefijo "/activity" 
 
 module.exports = app;
--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -3,12 +3,9 @@ const router = express.Router();
 
 const { validateRegister } = require("../middlewares/authMiddleware");
 //const registerController = require("../controllers/registerController");
-const activityController = require("../controllers/activityController"); 
 
 // Ruta para registrar un operador / administrador
 //router.post("/register", validateRegister, registerController.register);
 
-// Ruta para registrar actividades
-router.post("/registerActivity", activityController.registerActivity); 
 
 module.exports = router;
