35a6c5ee6afd3182e46ca07f99ed0b929203b325
Mildred <mildred09b@gmail.com>
2025-03-04 22:01:39 -0600

    CRUD de ordenante

--- controllers/ordenanteController.js
+++ controllers/ordenanteController.js
@@ -3,15 +3,12 @@ const Ordenante = require("../models/ordenante");
 // Obtener todos los ordenantes
 exports.getAllOrdenantes = async (req, res) => {
     try {
-        // Buscar todos los documentos con el rol "Ordenante"
         const ordenantes = await Ordenante.find({ Rol: "Ordenante" });
         
-        // Si no se encuentran ordenantes, se responde con un mensaje de error
         if (ordenantes.length === 0) {
             return res.status(404).json({ message: "No se encontraron ordenantes." });
         }
 
-        // Si se encuentran ordenantes, se responde con los datos
         res.status(200).json({ message: "Ordenantes encontrados.", data: ordenantes });
     } catch (error) {
         // En caso de error en la consulta, se captura y responde con error 500
@@ -25,25 +22,21 @@ exports.deleteOrdenante = async (req, res) => {
     const { RFCOrdenante } = req.params;  // Obtenemos el RFC del ordenante desde los parámetros de la ruta
     
     try {
-        // Buscar al ordenante por su RFC y solo recuperar el campo "Rol"
         const ordenante = await Ordenante.findOne({ RFCOrdenante }, { Rol: 1 });
         
-        // Si no existe un ordenante con ese RFC, responder con un error
         if (!ordenante) {
             return res.status(404).json({ message: "Ordenante no encontrado." });
         }
 
         // Si el ordenante es válido y tiene el rol "Ordenante", se procede a eliminarlo
         if (ordenante.Rol === "Ordenante") {
-            await Ordenante.deleteOne({ RFCOrdenante }); // Elimina al ordenante de la base de datos
+            await Ordenante.deleteOne({ RFCOrdenante });
             res.status(200).json({ message: "Ordenante eliminado con éxito." });
         } else {
-            // Si el rol del ordenante no es "Ordeante", se responde con un error
             console.error("No es ordenante.");
             res.status(403).json({ message: "No es ordenante." });
         }
     } catch (error) {
-        // Captura y responde con error en caso de fallo
         console.error("Error al eliminar el ordenante: ", error);
         res.status(500).json({ message: "Error interno del servidor." });
     }
@@ -54,15 +47,12 @@ exports.updateOrdenante = async (req, res) => {
     const { RFCOrdenante } = req.params;  // Obtenemos el RFC del ordenante desde los parámetros de la ruta
     
     try {
-        // Buscar al ordenante por su RFC y solo recuperar el campo "Rol"
         const ordenante = await Ordenante.findOne({ RFCOrdenante }, { Rol: 1 });
 
-        // Si no existe un ordenante con ese RFC, responder con un error
         if (!ordenante) {
             return res.status(404).json({ message: "Ordenante no encontrado." });
         }
 
-        // Si el ordenante es válido y tiene el rol "Ordenante", se procede a actualizar
         if (ordenante.Rol === "Ordenante") {
             // Extraemos los datos a actualizar desde el cuerpo de la solicitud
             const {
@@ -83,7 +73,7 @@ exports.updateOrdenante = async (req, res) => {
 
             // Actualizamos el documento con los nuevos valores
             await Ordenante.updateOne(
-                { RFCOrdenante }, // Filtramos por el RFC del ordenante
+                { RFCOrdenante }, 
                 { 
                     $set: {
                         "NombreOrdenante": NombreOrdenante,
@@ -105,20 +95,17 @@ exports.updateOrdenante = async (req, res) => {
                             }
                         ],
                         "Estado": Estado,
-                        "FechaUltimaModificacion": new Date() // Actualizamos la fecha de última modificación
+                        "FechaUltimaModificacion": new Date() 
                     }
                 }
             );
 
-            // Si la actualización fue exitosa, respondemos con éxito
             res.status(200).json({ message: "Ordenante actualizado con éxito." });
         } else {
-            // Si el rol del ordenante no es "Ordenante", respondemos con un error de autorización
             console.error("No es ordenante.");
             res.status(403).json({ message: "no es ordenante." });
         }
     } catch (error) {
-        // Captura y responde con error en caso de fallo
         console.log("Error al actualizar ordenante: ", error);
         res.status(500).json({ message: "Error interno del servidor." });
     }
--- routes/ordenanteRoutes.js
+++ routes/ordenanteRoutes.js
@@ -3,15 +3,11 @@ const router = express.Router();
 
 // Se importa el controlador para poder tener acceso a sus metodos
 const operadorController = require("../controllers/ordenanteController");
-const adminController = require("../controllers/ordenanteController");
 
-// Obtener a todos los ordenantes
 router.get("/getAllOrdenantes", operadorController.getAllOrdenantes);
 
-// Eliminar un ordenante
 router.delete("/deleteOrdenante/:RFCOrdenante", operadorController.deleteOrdenante);
 
-// Actualizar datos de un ordenante
 router.put("/updateOrdenante/:RFCOrdenante", operadorController.updateOrdenante);
 
 module.exports = router;
