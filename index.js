db8452ac8e9645db79b61948641b1816f595c1bc
USER1FR0 <f51783145@gmail.com>
2025-03-08 15:47:09 -0600

    fix/arreglar conflictos

--- index.js
+++ index.js
@@ -10,24 +10,27 @@ const adminRoutes = require('./routes/adminRoutes.js') // Importa las rutas de a
 const operatorRoutes = require('./routes/operatorRoutes.js'); // Importa las rutas de operador
 const activityRoutes = require('./routes/activityRoutes.js'); // Importa las rutas de activity
 const authRoutes = require('./routes/auth.js'); // Importa las rutas de autenticación
-const operatorRoutes = require('./routes/operatorRoutes.js');
 const registerRoutes = require('./routes/registerRoutes.js');
 const filtroRoutes = require('./routes/filtrosRoutes.js');
+const superRoutes = require('./routes/superRoutes.js');
 
 const app = express();
 
-//Middlewares
-
+// Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //Rutas
-app.use('/auth', authRoutes); // Monta las rutas bajo el prefijo "/auth"
 
-app.use('/admin', adminRoutes); // Monta las rutas bajo el prefijo "/admin"
-app.use('/operator', operatorRoutes); // Monta las rutas bajo el prefijo "/operator"
 app.use('/activity',activityRoutes); // Monta las rutas bajo el prefijo "/activity" 
 app.use('/filtros', filtroRoutes); // Monta las rutas bajo el prefijo "/filtros"
+
+// Rutas
+app.use('/admin', adminRoutes); // Prefijo "/admin"
+app.use('/operator', operatorRoutes); // Prefijo "/operator"
+app.use('/register', registerRoutes); // Prefijo "/register"
+app.use('/auth', authRoutes); // Prefijo "/auth"
+app.use('/superadmin', superRoutes); // Ahora coincide con "/superadmin/"
 
 module.exports = app;
